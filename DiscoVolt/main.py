964b12f2ad32d3820df05bdd30a5a9e0f66f7ce5
TheEdward <76453154+TheEdward0@users.noreply.github.com>
2023-06-12 16:51:54 +0330

    Update main.py

--- DiscoVolt/main.py
+++ DiscoVolt/main.py
@@ -2,7 +2,7 @@ import discord, os, alive, asyncio, datetime, pytz
 
 from discord.ext import tasks, commands
 
-client = commands.Bot(command_prefix='!', self_bot=True)
+client = commands.Bot(command_prefix='$', self_bot=True)
 
 
 # name = for your status and url = for your twitch link
@@ -12,6 +12,35 @@ async def on_connect():
         name="Hey Im Edward",
         url="https://www.youtube.com/watch?v=3qJWEFeelLE"))
 
+
+class color:
+    Purple = '\033[95m'
+    RED = '\033[91m'
+    wormy = '\033[93m'
+
+
+print(color.Purple + """
+
+░██████╗██╗░░██╗░█████╗░████████╗███████╗██████╗░██████╗░██╗███████╗
+██╔════╝██║░░██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██║██╔════╝
+╚█████╗░███████║██║░░██║░░░██║░░░█████╗░░██║░░██║██║░░██║██║█████╗░░
+░╚═══██╗██╔══██║██║░░██║░░░██║░░░██╔══╝░░██║░░██║██║░░██║██║██╔══╝░░
+██████╔╝██║░░██║╚█████╔╝░░░██║░░░███████╗██████╔╝██████╔╝██║███████╗
+╚═════╝░╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚══════╝╚═════╝░╚═════╝░╚═╝╚══════╝
+""")
+
+
+def printLow(Str):
+    for char in Str:
+        print(char, end='', flush=True)
+
+
+printLow(color.Purple + """
+Discord : discord.gg/hTP2DaMCnD
+Youtube : youtube.com/@ShotEddie
+
+
+""")
 
 alive.alive()
 client.run(os.getenv("TOKEN"), bot=False)
